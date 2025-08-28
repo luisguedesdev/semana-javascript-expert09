e4579568994a3e93418040f6b8f43bd5f2cb4860
Luis Guedes Dev <luisgcostaguedes@gmail.com>
2025-08-28 16:52:21 -0300

    finish day 2

--- sdk/src/controllers/chatBotController.js
+++ sdk/src/controllers/chatBotController.js
@@ -6,52 +6,78 @@
  */
 
 export class ChatbotController {
+  #chatbotView;
+  #promptService;
 
-    #chatbotView;
-    #promptService;
+  /**
+   * @param {Object} deps - Dependencies for the class.
+   * @param {ChatBotView} deps.chatbotView - The chatbot view instance.
+   * @param {PromptService} deps.promptService - The prompt service instance.
+   */
+  constructor({ chatbotView, promptService }) {
+    this.#chatbotView = chatbotView;
+    this.#promptService = promptService;
+  }
 
-    /**
-    * @param {Object} deps - Dependencies for the class.
-    * @param {ChatBotView} deps.chatbotView - The chatbot view instance.
-    * @param {PromptService} deps.promptService - The prompt service instance.
-    */
-    constructor({ chatbotView, promptService }) {
-        this.#chatbotView = chatbotView;
-        this.#promptService = promptService;
+  async init({ firstBotMessage, text }) {
+    this.#setupEvents();
+    this.#chatbotView.renderWelcomeBubble();
+    this.#chatbotView.setInputEnabled(true);
+    this.#chatbotView.appendBotMessage(firstBotMessage, null, false);
+    return this.#promptService.init(text);
+  }
+
+  #setupEvents() {
+    this.#chatbotView.setupEventHandlers({
+      onOpen: this.#onOpen.bind(this),
+      onSend: this.#chatBotReply.bind(this),
+      onStop: this.#handleStop.bind(this),
+    });
+  }
+
+  #handleStop() {}
+
+  async #chatBotReply(userMsg) {
+    console.log("received", userMsg);
+    this.#chatbotView.showTypingIndicator();
+    this.#chatbotView.setInputEnabled(false);
+    const response = await this.#promptService.prompt(userMsg);
+    console.log("response", response);
+
+    this.#chatbotView.appendBotMessage(response);
+    this.#chatbotView.setInputEnabled(true);
+    this.#chatbotView.hideTypingIndicator();
+  }
+
+  async #onOpen() {
+    const errors = this.#checkRequirements();
+    if (errors.length) {
+      const messages = errors.join("\n\n");
+      this.#chatbotView.appendBotMessage(messages);
+      this.#chatbotView.setInputEnabled(false);
+      return;
     }
+    this.#chatbotView.setInputEnabled(true);
+  }
 
-    async init({ firstBotMessage, text }) {
-        this.#setupEvents();
-        this.#chatbotView.renderWelcomeBubble();
-        this.#chatbotView.setInputEnabled(true);
-        this.#chatbotView.appendBotMessage(firstBotMessage, null, false);
+  #checkRequirements() {
+    const errors = [];
+    // @ts-ignore
+    // Serve para validar se o usuário está usando o navegador Chrome
+    const iChrome = window.chrome;
+    if (!iChrome) {
+      errors.push(
+        "⚠️ Este recurso só funciona no Google Chrome ou Chrome Canary (versão recente)."
+      );
     }
-
-    #setupEvents() {
-        this.#chatbotView.setupEventHandlers({
-            onOpen: this.#onOpen.bind(this),
-            onSend: this.#chatBotReply.bind(this),
-            onStop: this.#handleStop.bind(this),
-        });
+    if (!("LanguageModel" in window)) {
+      errors.push("⚠️ As APIs nativas de IA não estão ativas.");
+      errors.push("Ative a seguinte flag em chrome://flags/:");
+      errors.push(
+        "- Prompt API for Gemini Nano (chrome://flags/#prompt-api-for-gemini-nano)"
+      );
+      errors.push("Depois reinicie o Chrome e tente novamente.");
     }
-
-    #handleStop() {
-    }
-
-    async #chatBotReply(userMsg) {
-        console.log('received', userMsg)
-        this.#chatbotView.showTypingIndicator();
-        this.#chatbotView.setInputEnabled(false);
-        setTimeout(() => {
-            this.#chatbotView.appendBotMessage("Opa! Ainda não estou pronto para isso.", null, false);
-            this.#chatbotView.setInputEnabled(true);
-            this.#chatbotView.hideTypingIndicator();
-        }, 500);
-
-    }
-
-    async #onOpen() {
-        this.#chatbotView.setInputEnabled(true);
-    }
-
+    return errors;
+  }
 }
